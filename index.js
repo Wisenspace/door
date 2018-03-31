d495e92661f5280cd996a12ced4022fb70fa3cbd
Wisenspace <dev@wisen.space>
2018-03-31 14:26:25 -0700

    added user form

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* The express module is used to look at the address of the request and send it to the correct function */
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var usermodel = require('./user.js').getModel();
 
 /* The http module is used to listen for requests from a web browser */
 var http = require('http');
@@ -17,37 +19,44 @@ var server = http.createServer(app);
 /* Defines what port to use to listen to web requests */
 var port =  process.env.PORT ? parseInt(process.env.PORT) : 8080;
 
-app.use(bodyParser.json({ limit: '16mb' }));
+var dbAddress = process.env.MONGODB_URI || 'mongodb://127.0.0.1/game';
 
-/* Defines what function to call when a request comes from the path '/' in http://localhost:8080 */
-app.get('/form', (req, res, next) => {
+function startServer() {
+  app.use(bodyParser.json({ limit: '16mb' }));
 
-	/* Get the absolute path of the html file */
-	var filePath = path.join(__dirname, './index.html')
+  /* Defines what function to call when a request comes from the path '/' in http://localhost:8080 */
+  app.get('/form', (req, res, next) => {
 
-	/* Sends the html file back to the browser */
-	res.sendFile(filePath);
-});
+  	/* Get the absolute path of the html file */
+  	var filePath = path.join(__dirname, './index.html')
 
+  	/* Sends the html file back to the browser */
+  	res.sendFile(filePath);
+  });
 
-app.post('/form', (req, res, next) => {
-  console.log(req.body);
-  res.send('OK')
-})
+  app.post('/form', (req, res, next) => {
+    var newuser = new usermodel(req.body);
+    newuser.save(function(err) {
+      res.send(err || 'OK');
+    })
+  })
 
-/* Defines what function to all when the server recieves any request from http://localhost:8080 */
-server.on('listening', () => {
+  /* Defines what function to all when the server recieves any request from http://localhost:8080 */
+  server.on('listening', () => {
 
-	/* Determining what the server is listening for */
-	var addr = server.address()
-		, bind = typeof addr === 'string'
-			? 'pipe ' + addr
-			: 'port ' + addr.port
-	;
+  	/* Determining what the server is listening for */
+  	var addr = server.address()
+  		, bind = typeof addr === 'string'
+  			? 'pipe ' + addr
+  			: 'port ' + addr.port
+  	;
 
-	/* Outputs to the console that the webserver is ready to start listenting to requests */
-	console.log('Listening on ' + bind);
-});
+  	/* Outputs to the console that the webserver is ready to start listenting to requests */
+  	console.log('Listening on ' + bind);
+  });
 
-/* Tells the server to start listening to requests from defined port */
-server.listen(port);
+  /* Tells the server to start listening to requests from defined port */
+  server.listen(port);
+}
+
+mongoose.connect(dbAddress, startServer)
